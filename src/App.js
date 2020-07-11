503b8e06a744e3d504cf266bf84ee89e48f32aa3
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-07-11 17:46:24 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ROUTES, { RenderRoutes } from './routes';
+
+import MenuExample from './components/Menu.example';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'stretch' }}>
+      <div style={{ flex: 0.3, backgroundColor: '#f2f2f2' }}>
+        <MenuExample routes={ROUTES} />
+      </div>
+      <div>
+        <RenderRoutes routes={ROUTES} />
+      </div>
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const RouteWithSubRoutes = (route) => {
+  return (
+    <Route
+      path={route.path}
+      exact={route.exact}
+      render={(props) => <route.component {...props} routes={route.routes} />}
+    />
+  );
+};
+
+export const RenderRoutes = ({ routes }) => {
+  return (
+    <Switch>
+      {routes.map((route, i) => {
+        return <RouteWithSubRoutes key={route.key} {...route} />;
+      })}
+      <Route component={() => <h1>Not Found!</h1>} />
+    </Switch>
+  );
+};
+
+// Este es un arreglo donde irán todas las rutas de nuestra App ;)
+const ROUTES = [
+  { path: '/', key: 'ROOT', exact: true, component: () => <h1>Log in</h1> },
+  {
+    path: '/app',
+    key: 'APP',
+    component: RenderRoutes,
+    routes: [
+      {
+        path: '/app',
+        key: 'APP_ROOT',
+        exact: true,
+        component: () => <h1>App Index</h1>,
+      },
+      {
+        path: '/app/page',
+        key: 'APP_PAGE',
+        exact: true,
+        component: () => <h1>App Page</h1>,
+      },
+    ],
+  },
+];
+
+export default ROUTES;
