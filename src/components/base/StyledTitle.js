2e371e11bf8f591d020cd1f9fcb79d1a415efb0b
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-07-18 14:53:05 -0500

    Delete unused component

--- src/components/base/StyledTitle.js
+++ src/components/base/StyledTitle.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './../../sass/components/base/Title.scss';
 
+/**
+ * Renderiza el título doble.
+ *
+ * Colores soportados: [`primary`, `secondary`].
+ * Nivele soportados: [1-3]
+ */
 const StyledTitle = ({ text, level, color }) => {
   return (
     <div
-      className={`Title Title-${level}`}
-      style={{ color }}
+      className={`Title Title-${level} Color-${color}`}
       data-title={text}
     >
       {text}
