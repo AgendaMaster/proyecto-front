61810a29a42cdf9408a3b9add6f21c4831f55757
Razielini <razielco@gmail.com>
2020-07-18 19:36:54 -0600

    Update Calendar.js

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { DailyView } from '../components/Calendar/DailyView'
+import '../sass/components/calendar/Calendar.scss'
+
 
 export function Calendar() {
   return (
-    <div>
-      <h1>I'm an Calendar</h1>
+    <div className="Calendar">
+      <div className="titles">
+        <h2>Viernes &lt;&gt; - BTN DAY - BTN NEW </h2>
+      </div>
+      <div className="Calendar-container">
+        Calendar view DAY
+        {
+          <DailyView />
+        }
+      </div>
     </div>
   );
 }
