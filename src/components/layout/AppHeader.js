a7202f928b3269b1dff5658a6d9adf0769d48b18
narias1999 <nicolasarias870@gmail.com>
2020-07-20 21:17:11 -0500

    install font awesome

--- src/components/layout/AppHeader.js
+++ src/components/layout/AppHeader.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Logo from './../../assets/images/logo.svg';
 import './../../sass/components/layout/AppHeader.scss';
@@ -6,7 +8,7 @@ import './../../sass/components/layout/AppHeader.scss';
 export function AppHeader({ user = {} }) {
   return (
     <div className='AppHeader'>
-      <Link className='AppHeader-logo' to="/">
+      <Link className='AppHeader-logo' to='/'>
         <figure>
           <img src={Logo} alt='App logo (colorful brain)' />
         </figure>
@@ -21,7 +23,7 @@ export function AppHeader({ user = {} }) {
           <strong className='Acount-name'>{user.name}</strong>
         </div>
         <div className='Notifications'>
-          <i className='Notifications-icon'>Bell</i>
+          <FontAwesomeIcon className='Notifications-icon' icon={faBell} />
         </div>
       </div>
     </div>
