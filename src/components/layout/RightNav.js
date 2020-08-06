8158c683a278059c8e5977290743209a5bf059e6
Daniel Valderrama <dny9919@gmail.com>
2020-08-06 10:32:47 -0500

    fix nav punto de rutura 780px

--- src/components/layout/RightNav.js
+++ src/components/layout/RightNav.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './../../sass/components/layout/RightNav.scss';
 
 const RightNav = ({ open }) => {
   let openBtn = open ? "RightNav" : "RightNav-open";
   return (
       <ul className={openBtn}>
+        <Link  to='/'>
         <li>Inicio</li>
+      </Link>
         <li>Eventos</li>
         <li>Tickets</li>
         <li>Calendario</li>
